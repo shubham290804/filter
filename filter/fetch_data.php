fb5bc1f31e98114bc59084267c0d05df90954ef8
shubham290804 <164664663+shubham290804@users.noreply.github.com>
2024-03-29 14:55:05 +0530

    Add files via upload

---
+++ filter/fetch_data.php
@@ -0,0 +1,72 @@
+<?php
+
+//fetch_data.php
+
+include('database_connection.php');
+
+if(isset($_POST["action"]))
+{
+	$query = "
+		SELECT * FROM product WHERE product_status = '1'
+	";
+	if(isset($_POST["minimum_price"], $_POST["maximum_price"]) && !empty($_POST["minimum_price"]) && !empty($_POST["maximum_price"]))
+	{
+		$query .= "
+		 AND tlimit BETWEEN '".$_POST["minimum_price"]."' AND '".$_POST["maximum_price"]."'
+		";
+	}
+	if(isset($_POST["brand"]))
+	{
+		$brand_filter = implode("','", $_POST["brand"]);
+		$query .= "
+		 AND status IN('".$brand_filter."')
+		";
+	}
+	if(isset($_POST["ram"]))
+	{
+		$ram_filter = implode("','", $_POST["ram"]);
+		$query .= "
+		 AND size IN('".$ram_filter."')
+		";
+	}
+	if(isset($_POST["storage"]))
+	{
+		$storage_filter = implode("','", $_POST["storage"]);
+		$query .= "
+		 AND vlink IN('".$storage_filter."')
+		";
+	}
+
+	$statement = $connect->prepare($query);
+	$statement->execute();
+	$result = $statement->fetchAll();
+	$total_row = $statement->rowCount();
+	$output = '';
+	if($total_row > 0)
+	{
+		foreach($result as $row)
+		{
+			$output .= '
+			<div class="col-sm-4 col-lg-3 col-md-3">
+				<div style="border:1px solid #ccc; border-radius:5px; padding:16px; margin-bottom:16px; height:450px;">
+					<img src="image/'. $row['photo'] .'" alt="" class="img-responsive" >
+					<p align="center"><strong><a href="#">'. $row['Title'] .'</a></strong></p>
+					<h4 style="text-align:center;" class="text-danger" >'. $row['tlimit'] . 'min <br /></h4>
+					
+					Status : '. $row['status'] .' <br />
+					No. of Players: '. $row['size'] .' <br />
+					
+				</div>
+
+			</div>
+			';
+		}
+	}
+	else
+	{
+		$output = '<h3>No Data Found</h3>';
+	}
+	echo $output;
+}
+
+?>
